68ff6331b1bf629a9e82ae6b86a8988dc06cd1ad
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,105 +1,100 @@
-output "function_app_slots" {
-  description = "All function_app_slot resources"
-  value       = azurerm_function_app_slot.function_app_slots
-  sensitive   = true
-}
 output "function_app_slots_app_service_plan_id" {
-  description = "List of app_service_plan_id values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.app_service_plan_id]
+  description = "Map of app_service_plan_id values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.app_service_plan_id }
 }
 output "function_app_slots_app_settings" {
-  description = "List of app_settings values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.app_settings]
+  description = "Map of app_settings values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.app_settings }
 }
 output "function_app_slots_auth_settings" {
-  description = "List of auth_settings values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.auth_settings]
+  description = "Map of auth_settings values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.auth_settings }
   sensitive   = true
 }
 output "function_app_slots_connection_string" {
-  description = "List of connection_string values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.connection_string]
+  description = "Map of connection_string values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.connection_string }
   sensitive   = true
 }
 output "function_app_slots_daily_memory_time_quota" {
-  description = "List of daily_memory_time_quota values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.daily_memory_time_quota]
+  description = "Map of daily_memory_time_quota values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.daily_memory_time_quota }
 }
 output "function_app_slots_default_hostname" {
-  description = "List of default_hostname values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.default_hostname]
+  description = "Map of default_hostname values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.default_hostname }
 }
 output "function_app_slots_enable_builtin_logging" {
-  description = "List of enable_builtin_logging values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.enable_builtin_logging]
+  description = "Map of enable_builtin_logging values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.enable_builtin_logging }
 }
 output "function_app_slots_enabled" {
-  description = "List of enabled values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.enabled]
+  description = "Map of enabled values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.enabled }
 }
 output "function_app_slots_function_app_name" {
-  description = "List of function_app_name values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.function_app_name]
+  description = "Map of function_app_name values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.function_app_name }
 }
 output "function_app_slots_https_only" {
-  description = "List of https_only values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.https_only]
+  description = "Map of https_only values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.https_only }
 }
 output "function_app_slots_identity" {
-  description = "List of identity values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.identity]
+  description = "Map of identity values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.identity }
 }
 output "function_app_slots_kind" {
-  description = "List of kind values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.kind]
+  description = "Map of kind values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.kind }
 }
 output "function_app_slots_location" {
-  description = "List of location values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.location]
+  description = "Map of location values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.location }
 }
 output "function_app_slots_name" {
-  description = "List of name values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.name]
+  description = "Map of name values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.name }
 }
 output "function_app_slots_os_type" {
-  description = "List of os_type values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.os_type]
+  description = "Map of os_type values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.os_type }
 }
 output "function_app_slots_outbound_ip_addresses" {
-  description = "List of outbound_ip_addresses values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.outbound_ip_addresses]
+  description = "Map of outbound_ip_addresses values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.outbound_ip_addresses }
 }
 output "function_app_slots_possible_outbound_ip_addresses" {
-  description = "List of possible_outbound_ip_addresses values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.possible_outbound_ip_addresses]
+  description = "Map of possible_outbound_ip_addresses values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.possible_outbound_ip_addresses }
 }
 output "function_app_slots_resource_group_name" {
-  description = "List of resource_group_name values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.resource_group_name]
+  description = "Map of resource_group_name values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.resource_group_name }
 }
 output "function_app_slots_site_config" {
-  description = "List of site_config values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.site_config]
+  description = "Map of site_config values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.site_config }
 }
 output "function_app_slots_site_credential" {
-  description = "List of site_credential values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.site_credential]
+  description = "Map of site_credential values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.site_credential }
 }
 output "function_app_slots_storage_account_access_key" {
-  description = "List of storage_account_access_key values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.storage_account_access_key]
+  description = "Map of storage_account_access_key values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.storage_account_access_key }
   sensitive   = true
 }
 output "function_app_slots_storage_account_name" {
-  description = "List of storage_account_name values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.storage_account_name]
+  description = "Map of storage_account_name values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.storage_account_name }
 }
 output "function_app_slots_tags" {
-  description = "List of tags values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.tags]
+  description = "Map of tags values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.tags }
 }
 output "function_app_slots_version" {
-  description = "List of version values across all function_app_slots"
-  value       = [for k, v in azurerm_function_app_slot.function_app_slots : v.version]
+  description = "Map of version values across all function_app_slots, keyed the same as var.function_app_slots"
+  value       = { for k, v in azurerm_function_app_slot.function_app_slots : k => v.version }
 }
 
